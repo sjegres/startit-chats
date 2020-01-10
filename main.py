481c561d2e6aca5a04a89455497a87d125447945
sjegres <54534605+sjegres@users.noreply.github.com>
2020-01-10 14:38:23 +0200

    message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify, request
 
 
 app = Flask('app')
@@ -12,7 +12,23 @@ def index_page():
 def health_page():
   return "OK"
 
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  lines = []
+  fh = open('chats.txt', "r", encoding="UTF-8")
+  for line in fh:
+    lines.append(line)
+  fh.close()
+  return jsonify({"Chats":lines})
+
+@app.route('/chats/suuti', methods = ['POST'])
+def suutiit_zinju():
+  dati = request.json
+  with open("chats.txt", "a", newline="") as f:
+    f.write(dati["chats"] + "\n")
+  return ielasit_chatu()
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
